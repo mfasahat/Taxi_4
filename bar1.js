1cc09d47930823afc9e883527f18ba4bcdfe7562
mfasahat <78990632+mfasahat@users.noreply.github.com>
2021-05-11 20:55:54 -0400

    a

--- bar1.js
+++ bar1.js
@@ -57,7 +57,7 @@ function chart(data) {
 
     svg.append("text")
         .attr("transform", "rotate(-90)")
-        .attr("y", 0 - margin.left)
+        .attr("y", 20 - margin.left)
         .attr("x",0 - (height / 2))
         .attr("dy", "1em")
         .style("text-anchor", "middle")
